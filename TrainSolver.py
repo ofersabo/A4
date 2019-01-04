5063e6d5976a87e3003c45736f8976fbd0223a6a
ofersabo <saboof@post.bgu.ac.il>
2019-01-04 12:58:51 +0200

    svc ready with prec 0.55

---
+++ TrainSolver.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sys
+from sklearn.datasets import load_svmlight_file
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+import pickle
+
+
+# def only_check(model_file,X):
+#     model = load_model(model_file)
+#     print model.predict_score(X[4000:4001,:])
+#     print
+#     print X[4000:4001,:]
+#     exit()
+
+
+def main(feature_vec = "vec_file.txt", model_file = "saved_model_short"):
+
+    X_train, y_train = load_svmlight_file(feature_vec)
+    print ("loaded")
+    #only_check(model_file,X_train)
+    clf = LinearSVC(penalty='l2',verbose=True,C=2.5,loss='hinge')
+    model = clf.fit(X_train,y_train)
+    pickle.dump(clf, open(model_file, 'wb'))
+    return model_file
+
+if __name__ == '__main__':
+    main()
+
